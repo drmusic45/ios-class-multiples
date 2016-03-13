e1bf0e1b4acff075f25a6b02d71a11064907efdb
Edward A Williams <drmusic45@gmail.com>
2016-03-13 15:10:18 -0600

    Finished game

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,14 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Variables
+    var multiple:Int = 0
+    var answer:Int = 0
+    var prevAnswer:Int = 0
+    let limit:Int = 100
+    
+    //Outlets
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var multipleEntryField: UITextField!
+    @IBOutlet weak var multipleDisplay: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    //Functions
+    
+    func resetGame() {
+        titleImage.hidden = false
+        multipleEntryField.hidden = false
+        playButton.hidden = false
+        
+        addButton.hidden = true
+        multipleDisplay.hidden = true
+        
+        multiple = 0
+        answer = 0
+        prevAnswer = 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startGame(sender:UIButton!) {
+        
+        if multipleEntryField.text != nil && multipleEntryField.text != "" {
+            
+            titleImage.hidden = true
+            multipleEntryField.hidden = true
+            playButton.hidden = true
+            
+            addButton.hidden = false
+            multipleDisplay.hidden = false
+            
+            multiple = Int(multipleEntryField.text!)!
+            updateLabel(0, 0)
+        }
+        else {
+            multipleEntryField.placeholder = "Please enter a number"
+        }
+       
+    }
+    
+    @IBAction func addMultiple(sender:UIButton!) {
+        if (answer + multiple) >= limit {
+            resetGame()
+        }
+        else {
+            prevAnswer = answer
+            answer = prevAnswer + multiple
+            updateLabel(prevAnswer, answer)
+        }
+        
+    }
+    
+    func updateLabel(oldAnswer:Int, _ newAnswer:Int) {
+        multipleDisplay.text = "\(oldAnswer) + \(multiple) = \(newAnswer)"
+        
     }
 
 
